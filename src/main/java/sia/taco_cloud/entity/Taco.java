30b235400c9aef2dda004214c71ad97869ed9103
Volha <sakovolga@live.com>
2024-12-02 13:14:16 +0100

    create DesignTaco Page

---
+++ src/main/java/sia/taco_cloud/entity/Taco.java
@@ -0,0 +1,24 @@
+package sia.taco_cloud.entity;
+
+import java.util.List;
+
+public class Taco {
+    private String name;
+    private List<Ingredient> ingredients;
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
